0a909e485e5b3a01d232289987720102e189da80
PreethikaSubramani <preethikasubramani02@gmail.com>
2026-06-01 15:30:16 +0100

    message

--- src/Layout/Home.tsx
+++ src/Layout/Home.tsx
@@ -1,18 +1,31 @@
 import { useNavigate } from "react-router-dom";
-
+import "./Home.scss";
 
 const Home = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    <div className="home-container" >
-      <h1>Home</h1>
-      <p>Welcome to the Home page!</p>
-      <button onClick={() => navigate('/login')} className="btn">
-        Login
-      </button>
-      <button onClick={() => navigate('/register')} className="btn">
-        Register
-        </button>
+    <div className="home-container">
+      
+      <div className="welcom">
+        <h1>Welcome Preethika </h1>
+      </div>
+
+      <div>
+        <div className="application">
+          <button onClick={() => navigate("/application")} className="btn">
+            + Application
+          </button>
+        </div>
+        <div className="auth">
+          <button onClick={() => navigate("/login")} className="btn">
+            Login
+          </button>
+          <button onClick={() => navigate("/register")} className="btn">
+            Register
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 };
